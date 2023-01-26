15ee6af7957caab2cb5b426121295902f2ceed0e
obed7 <obedtp21@gmail.com>
2023-01-25 21:15:36 -0600

    probando commit en github

--- src/main/java/net/maruchan/tutorialmod/TutorialMod.java
+++ src/main/java/net/maruchan/tutorialmod/TutorialMod.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	//probando github
 	@Override
 	public void onInitialize() {
 	}
