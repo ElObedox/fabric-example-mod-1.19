147f3f91bc26ad487387bc530bdbf684c7199af1
obed7 <obedtp21@gmail.com>
2023-01-26 20:42:23 -0600

    cambiando a version 1.19

---
+++ src/main/java/net/obed/modprueba/ModPrueba.java
@@ -0,0 +1,16 @@
+package net.obed.modprueba;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModPrueba implements ModInitializer {
+
+	public static final String MOD_ID = "modprueba";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
